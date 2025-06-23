86ece479a643f5daf20ff3c1d1577f0194b06e4d
hanx <i.hankxiong@outlook.com>
2025-06-23 16:55:00 +0800

    实现登录注册逻辑

---
+++ patient-service/src/main/java/com/niit/patientservice/service/LoginService.java
@@ -0,0 +1,23 @@
+package com.niit.patientservice.service;
+
+import com.niit.patientservice.dao.PatientRepository;
+import com.niit.patientservice.entity.Patient;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+
+    final PatientRepository patientRepository;
+
+    public LoginService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public Patient login(String username, String password) {
+        return patientRepository.findByUsernameAndPassword(username, password);
+    }
+
+    public Patient register(Patient patient) {
+        return patientRepository.saveAndFlush(patient);
+    }
+}
