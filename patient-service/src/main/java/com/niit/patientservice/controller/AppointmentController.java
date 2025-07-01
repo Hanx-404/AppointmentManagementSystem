2bd8a9473cd52e391f98b1253c88dd361841acbc
Hanx-404 <i.hankxiong@outlook.com>
2025-07-01 15:37:20 +0800

    合并 schedule-table 分支，格式化代码，删除被注释掉的废弃代码

--- patient-service/src/main/java/com/niit/patientservice/controller/AppointmentController.java
+++ patient-service/src/main/java/com/niit/patientservice/controller/AppointmentController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDate;
-import java.util.Objects;
 
 @Controller
 public class AppointmentController {
--- patient-service/src/main/java/com/niit/patientservice/controller/IndexController.java
+++ patient-service/src/main/java/com/niit/patientservice/controller/IndexController.java
@@ -1,8 +1,6 @@
 package com.niit.patientservice.controller;
 
-import com.niit.patientservice.service.AppointmentService;
 import com.niit.patientservice.service.ClinicService;
-import com.niit.patientservice.service.DoctorService;
 import com.niit.patientservice.service.ScheduleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,14 +13,10 @@ import java.time.LocalDate;
 public class IndexController {
 
     private final ClinicService clinicService;
-    private final DoctorService doctorService;
-    private final AppointmentService appointmentService;
     private final ScheduleService scheduleService;
 
-    public IndexController(ClinicService clinicService, DoctorService doctorService, AppointmentService appointmentService, ScheduleService scheduleService) {
+    public IndexController(ClinicService clinicService, ScheduleService scheduleService) {
         this.clinicService = clinicService;
-        this.doctorService = doctorService;
-        this.appointmentService = appointmentService;
         this.scheduleService = scheduleService;
     }
 
@@ -38,21 +32,10 @@ public class IndexController {
         return "index";
     }
 
-    // @GetMapping("/clinic/{clinicId}")
-    // public String clinic(@PathVariable int clinicId, Model model) {
-    //     model.addAttribute("timeSlots", appointmentService.generateTimeSlots());
-    //     model.addAttribute("doctors", doctorService.getDoctorsByClinicId(clinicId));
-    //     return "registration";
-    // }
-
     @GetMapping("/registration/{clinicId}")
     public String registrationDefault(@PathVariable int clinicId, Model model) {
         LocalDate date = LocalDate.now();
         char time = 'A';
-        // model.addAttribute("clinicId", clinicId);
-        // model.addAttribute("timeSlots", scheduleService.generateTimeSlots());
-        // model.addAttribute("schedule", scheduleService.getSchedulesByClinicIdAndDateAndTime(clinicId, date, time));
-        // return "registration";
         return "redirect:/registration/" + clinicId + "/" + date + "/" + time;
     }
 
